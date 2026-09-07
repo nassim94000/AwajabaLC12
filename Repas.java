cc0bddb6f239eefcd70a03bee9ab3b88c8a4fcf9
devadmin <devadmin@VM-DEV-WEB.BTSSIO.local>
2026-09-07 10:43:11 +0200

    implementation du constructeur Repas (int, LocalDate)

--- Repas.java
+++ Repas.java
@@ -3,6 +3,11 @@ import java.time.LocalDate ;
 
 public class Repas {
 	private int numeros;
-	private LocalDate 
+	private LocalDate ;
 	
+	public Repas ( int numero , LocalDate date) {
+		super()
+		this.numeros = numero ;
+		this.date = date ;
+	}
 }
